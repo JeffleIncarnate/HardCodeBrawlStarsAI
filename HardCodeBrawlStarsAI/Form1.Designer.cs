1a6cec7d616709a78526f149a238bc26dd4e8ec4
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-04-29 16:12:06 +1200

    Multi Threading and Seeing phone screen

--- HardCodeBrawlStarsAI/Form1.Designer.cs
+++ HardCodeBrawlStarsAI/Form1.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.scrcpy = new System.Windows.Forms.Button();
+            this.capture = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // scrcpy
+            // 
+            this.scrcpy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.scrcpy.Location = new System.Drawing.Point(23, 21);
+            this.scrcpy.Name = "scrcpy";
+            this.scrcpy.Size = new System.Drawing.Size(113, 46);
+            this.scrcpy.TabIndex = 0;
+            this.scrcpy.Text = "Start Scrcpy";
+            this.scrcpy.UseVisualStyleBackColor = true;
+            this.scrcpy.Click += new System.EventHandler(this.scrcpy_Click);
+            // 
+            // capture
+            // 
+            this.capture.Location = new System.Drawing.Point(160, 21);
+            this.capture.Name = "capture";
+            this.capture.Size = new System.Drawing.Size(135, 46);
+            this.capture.TabIndex = 1;
+            this.capture.Text = "Capture Screen";
+            this.capture.UseVisualStyleBackColor = true;
+            this.capture.Click += new System.EventHandler(this.capture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(365, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1338, 682);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1715, 743);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.capture);
+            this.Controls.Add(this.scrcpy);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button scrcpy;
+        private System.Windows.Forms.Button capture;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- HardCodeBrawlStarsAI/Form1.cs
+++ HardCodeBrawlStarsAI/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace HardCodeBrawlStarsAI
 {
@@ -17,9 +21,49 @@ namespace HardCodeBrawlStarsAI
             InitializeComponent();
         }
 
+        // Before Form is loaded
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void scrcpy_Click(object sender, EventArgs e)
+        {
+            string Scrcpy = @"C:\Users\hp\Downloads\scrcpy-win64-v1.23\scrcpy.exe";
+            Process.Start(Scrcpy);
+        }
+
+        private void capture_Click(object sender, EventArgs e)
+        {
+            ThreadStart ChildRef = new ThreadStart(CaptureMyScreen);
+            Thread ChildThread = new Thread(ChildRef);
+            ChildThread.Start();
+        }
+
+        public void CaptureMyScreen()
+        {
+            try
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    //Creating a new Bitmap object
+                    Bitmap captureBitmap = new Bitmap(1024, 768, PixelFormat.Format32bppArgb);
+                    //Bitmap captureBitmap = new Bitmap(int width, int height, PixelFormat);
+                    //Creating a Rectangle object which will
+                    //capture our Current Screen
+                    Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+                    //Creating a New Graphics Object
+                    Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+                    //Copying Image from The Screen
+                    captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+
+                    pictureBox1.Image = captureBitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
