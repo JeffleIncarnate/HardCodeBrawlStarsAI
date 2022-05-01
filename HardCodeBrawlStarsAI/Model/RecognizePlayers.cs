6d972f81ab1a59b944bc035e854ec5e79b4720db
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-05-01 12:42:23 +1200

    Added Library Folder

---
+++ HardCodeBrawlStarsAI/Model/RecognizePlayers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardCodeBrawlStarsAI.Model
+{
+    internal class RecognizePlayers
+    {
+        /// <summary>
+        /// 1. Create a Custom Training Set
+        /// 2. Train a Custom Model
+        /// 3. Deploy to Code
+        /// </summary>
+    }
+}
